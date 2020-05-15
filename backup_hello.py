fa706fad5842b1b88c4e0e364e09fc8f6ce95044
carls <carls@ip-172-31-93-109.ec2.internal>
2020-05-15 19:11:36 +0000

    sending

---
+++ backup_hello.py
@@ -0,0 +1,17 @@
+# This is my first python program
+
+name="Raj"
+age=22
+
+brothername="Vijay"
+brotherage=26
+
+agedifference=age-brotherage
+difference=abs(agedifference)
+
+print("My name is: ",name)
+print("My age is: ",age)
+print("My brother's name is:",brothername)
+print("My brother's age is:",brotherage)
+print("We were born",difference,"years apart")
+
